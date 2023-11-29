2de6adbc4d0c0c4827e3a3192c4aa79d749383c9
Joshua Elijah Pura <business.joshua.pura@gmail.com>
2023-11-29 18:15:14 +0800

    First Upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//DOM Manipulation
+
+//getElementById()
+const title = document.getElementById('main-heading');
+console.log(title)
+
+//GetElementByClassName()
+
+const listItemClass = document.getElementsByClassName('list-items')
+console.log(listItemClass);
+
+//GetElementByTagName()
+const listItem = document.getElementsByTagName('li');
+console.log(listItem);
+
+//querySelector()
+const container = document.querySelector('div');
+console.log(container);
+
+//querySelectorAll()
+const allContainer = document.querySelectorAll('div');
+console.log(allContainer);
