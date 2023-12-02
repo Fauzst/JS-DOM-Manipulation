c8f9a0077ede6e8e789b9ef3acdd2aa45266e13e
Joshua Elijah Pura <business.joshua.pura@gmail.com>
2023-12-02 22:50:34 +0800

    4th Commit, Dec 2, 2023, Saturday

--- app.js
+++ app.js
@@ -30,35 +30,51 @@ console.log(allContainer);
 
 //Create Element
 //Below, I created a constant named ul and li. First, it selected tag ul to be passed on the const ul. Secondly, the we created a li element on the const li
-const ul = document.querySelector('ul');
-const li = document.createElement('li');
+//const ul = document.querySelector('ul');
+//const li = document.createElement('li');
 //Creates an empty li
 //Now after we created that, the ul which was selected by the query selector was appended with the newly created li.
-ul.append(li);
+//ul.append(li);
 //Adding something inside the li
-const firstListItem = document.querySelector('.list-items')
-console.log(firstListItem.innerText);
+//const firstListItem = document.querySelector('.list-items')
+//console.log(firstListItem.innerText);
 
-li.innerText = 'Cardo';
+//li.innerText = 'Cardo';
 //modify atrributes and classes
-li.setAttribute('id', 'main-heading');
-li.removeAttribute('id');
+//li.setAttribute('id', 'main-heading');
+//li.removeAttribute('id');
 
-const title = document.querySelector('#main-heading');
-console.log(title.getAttribute('id'));
+//const title = document.querySelector('#main-heading');
+//console.log(title.getAttribute('id'));
 
 //add or remove class
-li.classList.add('list-items');
+//li.classList.add('list-items');
 //li.classList.add('list-items);
 
-console.log(li.classList.contains('list-items'));
+//console.log(li.classList.contains('list-items'));
 
 //Remove elements
-li.remove() //this removes 'Cardo' or the const li we made earlier
+//li.remove() //this removes 'Cardo' or the const li we made earlier
 
+//Parent note traversal
+//let ul = document.querySelector('ul')
+//the .parentNode will find the parent of the element, in this example the 'ul'. Using it twice will return the parent of the parent of 'ul'
+//console.log(ul.parentNode.parentNode)
 
+//const html = document.getElement;
+//console.log(html.parentNode);
 
-/* 
+let ul = document.querySelector('ul')
+//This function finds the child nodes of the 'ul' element.
+console.log(ul.childNodes);
+console.log(ul.firstChild)
+console.log(ul.lastChild)
+//This is how we can style a child node.
+ul.childNodes[1].style.backgroundColor = 'blue';
+
+console.log(ul.previousSibling);
+console.log(ul.nextSibling);
+/*
 LIST OF METHODS
 -document.getElementById('idhere');
 -document.getElementByClassName('classnamehere);
