349b2e2fc9092c6b999b48b047388641a01391b0
Joshua Elijah Pura <business.joshua.pura@gmail.com>
2023-12-07 15:03:37 +0800

    7th Commit: Event Probagation, December 7, 2023, Thursday

--- revealJS.js
+++ revealJS.js
@@ -3,6 +3,7 @@
 const revealBtn = document.querySelector('.reveal-btn');
 const hiddenContent = document.querySelector('.hidden-content');
 
+//A function which check if the hiddenContent constant contains reave;
 function revealContent() {
     if(hiddenContent.classList.contains('reveal-btn')){
         hiddenContent.classList.remove('reveal-btn')
